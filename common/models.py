e926f2d5c768753f8d3199e6e3dae5efae0f76ba
Hakim Boulahya <hakimblhya@gmail.com>
2017-11-20 02:36:48 +0100

    [common] Block and Transaction models

--- common/models.py
+++ common/models.py
@@ -2,7 +2,19 @@ from django.db import models
 
 
 class Block(models.Model):
-    hash = models.CharField(max_length=256) # SHA256 length
+    hash = models.CharField(max_length=256)  # SHA256 length
+    hashPrevBlock = models.CharField(max_length=256)  # SHA256 length
+    # nonce = models.IntegerField()
+
+    class JSONAPIMeta:
+        resource_name = "block"
+
+class Transaction(models.Model):
+    txid = models.CharField(max_length=256) # SHA256 length
+    value = models.FloatField()
+    receiver = models.CharField(max_length=256)
+    sender = models.CharField(max_length=256)
+    blockHash = models.CharField(max_length=256)
 
     class JSONAPIMeta:
         resource_name = "block"
