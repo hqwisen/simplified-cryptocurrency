8dc15fd24c3ac3e7ae59c5b425e9600ebaffdd96
huseyintektas <tektas-h@hotmail.com>
2017-11-21 19:49:25 +0100

    Creation of first block with mock transactions

--- common/models.py
+++ common/models.py
@@ -2,19 +2,23 @@ from django.db import models
 
 
 class Block(models.Model):
-    hashBlock = models.CharField(max_length=256)  # SHA256 length
-    hashPrevBlock = models.CharField(max_length=256)  # SHA256 length
-    # nonce = models.IntegerField()
+    header = models.CharField(max_length=256)  # SHA256 length
+    nonce = models.IntegerField()
+    # def __init__(self, *args, **kwargs):
+    #     super(models.Model, self).__init__(args, kwargs)
+    #     self.transactions = []
 
     class JSONAPIMeta:
         resource_name = "block"
 
+
 class Transaction(models.Model):
-    txid = models.CharField(max_length=256) # SHA256 length
+    txid = models.CharField(primary_key=True, max_length=256) # SHA256 length
     amount = models.FloatField()
     receiver = models.CharField(max_length=256)
     sender = models.CharField(max_length=256)
-    blockHash = models.CharField(max_length=256)
+    block_hash = models.CharField(max_length=256)
+    timestamp = models.CharField(max_length=256)
 
     class JSONAPIMeta:
         resource_name = "transaction"
--- miner/Miner.py
+++ miner/Miner.py
@@ -7,12 +7,22 @@
 
 
 import hashlib
+#import sys
+#sys.path.append('..')
+#from common import models
 
 class Miner:
 
 	def __init__(self, ID):
 		self.ID = ID 				# Identify the miner
 		self.transactions = list()	# List of transaction object
+
+	def startMining(self, difficulty):
+		# request transactions
+		# Verify transactions -- Signature + amount
+		complete_hash, nonce = self.proofOfWork(difficulty)
+		newBlock = self.createBlock(complete_hash, nonce)
+
 
 	def proofOfWork(self, difficulty):
 		"""
@@ -21,24 +31,76 @@ class Miner:
 		zeros determined by the difficulty at the start of string
 		"""
 
-		plaintext = "hahahha"		# Plaintext will be self.transactions transformed 
+		# mock - hash of the last block of the blockchain
+		hashPrec = "fzf54zfz5ef4z6ef4ze5f4zef54ze5f46zef4z54f56zef"
+
+		transactionsString = self.getStringOfTransactions()		# Plaintext will be self.transactions transformed 
 		nonce = 0
 		notFound = True
 
 		while(notFound):
-			hash_object = hashlib.sha256(str.encode(plaintext + str(nonce))) # b allows to concert string to binary
-			nonce +=1
-			hex_hash = hash_object.hexdigest()
-			print(hex_hash)
-			if(self.isAcceptedByDifficulty(hex_hash, difficulty)):
+			hash_object = hashlib.sha256(str.encode(hashPrec + transactionsString + str(nonce))) # b allows to concert string to binary
+			complete_hash = hash_object.hexdigest()
+			print(complete_hash)
+			if(self.isAcceptedByDifficulty(complete_hash, difficulty)):
 				notFound = False
+			else:
+				nonce+=1
 
+
+		return complete_hash, nonce
 
 	def isAcceptedByDifficulty(self, hash, difficulty):
 		firstCharac = hash[:difficulty]
 		return firstCharac == "0" * difficulty
 
+	def addTransaction(self, transaction):
+		self.transactions.append(transaction)
+
+	# Need all blockchain and change variable hashPrec
+	# Need transactions
+	def requestTransaction(self):
+		pass
+
+	def getStringOfTransactions(self):
+		s=""
+		for transaction in self.transactions:
+			s+= transaction.toString()
+		return s
+	
+	def createBlock(self, complete_hash, nonce):
+		return Block(complete_hash, nonce, self.transactions)
+
+
+# Only for testing
+class Transaction():
+
+	def __init__(self):
+		self.txid = "eee"
+		self.amount = 80
+		self.receiver = "BB"
+		self.sender = "AA"
+		self.signature = "signature" 
+		self.timestamp = "12/12/2012"
+
+	def toString(self):
+		return self.txid + str(self.amount) + self.receiver + self.sender + self.signature + self.timestamp
+
+class Block():
+
+	def __init__(self, header, nonce, transactions):
+		self.header = header
+		self.nonce = nonce
+		self.transactions = transactions
+
+
 
 if __name__ == "__main__":
 	miner = Miner(555)
-	miner.proofOfWork(5)
+	miner.addTransaction(Transaction())
+	miner.addTransaction(Transaction())
+	miner.addTransaction(Transaction())
+	miner.addTransaction(Transaction())
+	miner.addTransaction(Transaction())
+
+	miner.startMining(3)
