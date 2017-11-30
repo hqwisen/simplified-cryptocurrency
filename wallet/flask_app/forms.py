828097266048d4032a38146f3af183421d8782a4
Thomas Herman <hermanthomas9@outlook.com>
2017-11-30 22:13:48 +0100

    [add] create address form

--- wallet/flask_app/forms.py
+++ wallet/flask_app/forms.py
@@ -5,6 +5,10 @@ from wtforms.validators import DataRequired, Length, NumberRange
 class LoginForm(FlaskForm):
     password = PasswordField('password', validators=[Length(min=16, max=16), DataRequired()])
 
-class MakeTransaction(FlaskForm):
+class MakeTransactionForm(FlaskForm):
     receiver = StringField('receiver', validators=[DataRequired()])
     amount = FloatField('amount', validators=[NumberRange(min=0), DataRequired()])
+
+class CreateAddressForm(FlaskForm):
+    password = PasswordField('password', validators=[Length(min=16, max=16), DataRequired()])
+    label = StringField('label')
--- wallet/flask_app/views.py
+++ wallet/flask_app/views.py
@@ -4,7 +4,7 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.getcwd()))
 from wallet import Wallet, Transaction
-from .forms import LoginForm, MakeTransaction
+from .forms import LoginForm, MakeTransactionForm, CreateAddressForm
 
 WRONG_PASSWORD_ERROR = 'Wrong password'
 SIGN_TRANSACTION_SUCCESS = 'Transaction succesfully signed and sent'
@@ -18,24 +18,20 @@ if not os.path.exists(SAVE_DIR):
 def get_all_saved_addresses():
     return [file for file in os.listdir(SAVE_DIR) if os.path.isfile(os.path.join(SAVE_DIR, file))]
 
-addresses = get_all_saved_addresses()
 wallet = Wallet()
-
-def refresh():
-    addresses = get_all_saved_addresses()
 
 @app.route('/', methods=[GET, POST])
 @app.route('/index', methods=[GET, POST])
 def index():
     current_address = wallet.current_address
     label = current_address.label if current_address else None
-    form = MakeTransaction()
+    form = MakeTransactionForm()
     if form.validate_on_submit():
         transaction = wallet.create_transaction(form.receiver.data, form.amount.data)
         print(Transaction.serialize(transaction))
         flash(SIGN_TRANSACTION_SUCCESS, GREEN_ALERT)
         return redirect('/')
-    return render_template('index.html', addresses=addresses, current_address=current_address, label=label, form=form)
+    return render_template('index.html', addresses=get_all_saved_addresses(), current_address=current_address, label=label, form=form)
 
 @app.route('/login/<label>', methods=[GET, POST])
 def login(label):
@@ -47,14 +43,18 @@ def login(label):
         if wallet.log_in(str(form.password.data), str(label)):
             return redirect('/')
         error = WRONG_PASSWORD_ERROR
-    return render_template('login.html', addresses=addresses, label=label, error=error, form=form)
+    return render_template('login.html', addresses=get_all_saved_addresses(), label=label, error=error, form=form)
+
+@app.route('/create_address', methods=[GET, POST])
+def create_address():
+    form = CreateAddressForm()
+    label = wallet.current_address.label if wallet.current_address else None
+    if form.validate_on_submit():
+        new_address = wallet.sign_up(form.password.data, form.label.data)
+        return redirect('/')
+    return render_template('create_address.html', addresses=get_all_saved_addresses(), current_address=wallet.current_address, label=label, form=form)
 
 @app.route('/logout')
 def logout():
     wallet.log_out()
     return redirect('/')
-
-@app.route('/test')
-def ok():
-    wallet.log_in('aaaaaaaaaaaaaaaa', 'address1')
-    return index()
