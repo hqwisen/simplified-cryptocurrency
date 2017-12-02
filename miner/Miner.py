e52a15c2257355dfb3b837ecdc374b772b8f2ce6
huseyintektas <33665575+huseyintektas@users.noreply.github.com>
2017-12-02 19:02:44 +0100

    Finalization of miner

--- miner/Miner.py
+++ miner/Miner.py
@@ -7,42 +7,96 @@
 
 
 import hashlib
-#import sys
-#sys.path.append('..')
-#from common import models
+import sys
+sys.path.append('..')
+
+from common.models import*
+import requests
 
 class Miner:
 
-	def __init__(self, ID):
-		self.ID = ID 				# Identify the miner
+	# Static variable which represent the mining difficulty
+	DIFFICULTY = 5
+
+	def __init__(self, address, ip, port):
+		self.address = address 				# Identify the miner with an adress, this adress will be used for paiment by masternode
 		self.transactions = list()	# List of transaction object
+		self.ip = ip
+		self.port = port
+		self.url = "http://"+ip+":"+str(port)
+		self.startMining()
 
-	def startMining(self, difficulty):
-		# request transactions
-		# Verify transactions -- Signature + amount
-		complete_hash, nonce = self.proofOfWork(difficulty)
-		newBlock = self.createBlock(complete_hash, nonce)
-
-
-	def proofOfWork(self, difficulty):
+	def startMining(self):
 		"""
-		Allow to verify the work of the miner. 
-		It will check if the hash has a given number of 
+		Requests the 5 transactions, verifies them and proofOfWork with given difficulty
+		"""
+
+		while(True):
+			excludes = list()
+			balanceDic = dict()
+			blockchainJson = requests.get(self.url +"/relay/blockchain")
+			blockchain = BlockChain.parse(blockchainJson)
+			while(len(self.transactions) < 5):
+				data = {"exclude_hash":excludes}
+				transactionJson = requests.get(self.url +"/relay/transaction", data)
+				# If transaction is not empty ( None )
+				if not transactionJson:
+					balanceKeys = balanceDic.keys
+					transaction = Transaction.parse(transactionJson)
+					sender = transaction.get_sender()
+					receiver = transaction.get_receiver()
+
+					####################################################
+					# VERIF SIGNATURE
+					if(transaction.verify_signature()):
+						# VERIF BALANCE
+						if (not sender in balanceKeys):
+							balanceDic[transaction.sender] = blockchain.get_balance(sender)
+						balanceDic[transaction.sender] -= transaction.amount
+						if (not receiver in balanceKeys):
+							balanceDic[transaction.receiver] = blockchain.get_balance(receiver)
+						balanceDic[transaction.receiver] += transaction.amount
+
+						if (balanceDic.get(sender) >= 0):
+							self.transactions.append(transaction)
+						else:
+							balanceDic[transaction.sender] += transaction.amount
+							balanceDic[transaction.receiver] -= transaction.amount
+					# Add this transaction to our excludes
+					excludes.append(transaction.hash)
+					####################################################
+
+			# Once we got all transactions and verifications done.
+			# We can start PROOF OF WORK
+			# part_of(start, end) , we only need last block's hash
+			lastBlockchainHash = blockchain.part_of(len(blockchain) -1, len(blockchain)).header
+			complete_hash, nonce = self.proofOfWork(lastBlockchainHash)
+			newBlock = self.createBlock(complete_hash, nonce)
+			####################################################
+
+			# Send block to relay
+			# Create JSON with format of acceptance by relay. We give the address to get paid if block is accepted
+			blockAndaddress = {'block':newBlock, 'miner_address':self.address}
+			requests.post(self.url+"/relay/block", blockAndaddress)
+
+
+	def proofOfWork(self, lastBlockchainHash):
+		"""
+		Allow to verify the work of the miner.
+		It will check if the hash has a given number of
 		zeros determined by the difficulty at the start of string
 		"""
 
-		# mock - hash of the last block of the blockchain
-		hashPrec = "fzf54zfz5ef4z6ef4ze5f4zef54ze5f46zef4z54f56zef"
-
-		transactionsString = self.getStringOfTransactions()		# Plaintext will be self.transactions transformed 
+		transactionsString = self.getStringOfTransactions()		# Plaintext will be self.transactions transformed
 		nonce = 0
 		notFound = True
 
 		while(notFound):
-			hash_object = hashlib.sha256(str.encode(hashPrec + transactionsString + str(nonce))) # b allows to concert string to binary
+			hash_object = hashlib.sha256(str.encode(lastBlockchainHash + transactionsString + str(nonce))) # b allows to concert string to binary
 			complete_hash = hash_object.hexdigest()
-			print(complete_hash)
-			if(self.isAcceptedByDifficulty(complete_hash, difficulty)):
+
+			if(self.isAcceptedByDifficulty(complete_hash)):
+				print(complete_hash)
 				notFound = False
 			else:
 				nonce+=1
@@ -50,57 +104,33 @@ class Miner:
 
 		return complete_hash, nonce
 
-	def isAcceptedByDifficulty(self, hash, difficulty):
-		firstCharac = hash[:difficulty]
-		return firstCharac == "0" * difficulty
-
-	def addTransaction(self, transaction):
-		self.transactions.append(transaction)
-
-	# Need all blockchain and change variable hashPrec
-	# Need transactions
-	def requestTransaction(self):
-		pass
+	def isAcceptedByDifficulty(self, hash):
+		"""
+		Checks if difficulty is reached by given hash and difficulty
+		"""
+		firstCharac = hash[:DIFFICULTY]
+		return firstCharac == "0" * DIFFICULTY
 
 	def getStringOfTransactions(self):
+		"""
+		Creates string for the list of transactions
+		"""
 		s=""
 		for transaction in self.transactions:
 			s+= transaction.toString()
 		return s
-	
+
 	def createBlock(self, complete_hash, nonce):
-		return Block(complete_hash, nonce, self.transactions)
-
-
-# Only for testing
-class Transaction():
-
-	def __init__(self):
-		self.txid = "eee"
-		self.amount = 80
-		self.receiver = "BB"
-		self.sender = "AA"
-		self.signature = "signature" 
-		self.timestamp = "12/12/2012"
-
-	def toString(self):
-		return self.txid + str(self.amount) + self.receiver + self.sender + self.signature + self.timestamp
-
-class Block():
-
-	def __init__(self, header, nonce, transactions):
-		self.header = header
-		self.nonce = nonce
-		self.transactions = transactions
-
-
+		"""
+		Create and returns a Block with given complete_hash and nonce
+		"""
+		block = Block(complete_hash, nonce)
+		for transaction in self.transactions:
+			block.add_transaction(transaction)
+		return block
 
 if __name__ == "__main__":
-	miner = Miner(555)
-	miner.addTransaction(Transaction())
-	miner.addTransaction(Transaction())
-	miner.addTransaction(Transaction())
-	miner.addTransaction(Transaction())
-	miner.addTransaction(Transaction())
-
-	miner.startMining(3)
+	#TODO
+	#TODO
+	#TODO Give legit address ip of relay
+	miner = Miner('9878sd5f4sd5f4g89464', "localhost",5555)
